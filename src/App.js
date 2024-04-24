2d9fcd4399e9270ae1386385adb2b56226adc3cd
mahfuzurrah <mahfuzuruiux@gmail.com>
2024-04-24 21:36:39 +0600

    Add

--- src/App.js
+++ src/App.js
@@ -1,30 +1,24 @@
-import React from "react";
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import MainLayout from "./components/layouts/MainLayout";
-import Dashboard from "./pages/Dashboard";
-import ScrollToTopOnReload from "./components/ScrollToTopOnReload";
-import Models from "./pages/Models";
-import Wallet from "./pages/Wallet";
-import Profile from "./pages/Profile";
-import Datasets from "./pages/Datasets";
-
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <>
-      <Router>
-        <ScrollToTopOnReload />
-        <Routes>
-          <Route path="/" element={<MainLayout />}>
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/datasets" element={<Datasets />} />
-            <Route path="/models" element={<Models />} />
-            <Route path="/wallet" element={<Wallet />} />
-            <Route path="/Profile" element={<Profile />} />
-          </Route>
-        </Routes>
-      </Router>
-    </>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
