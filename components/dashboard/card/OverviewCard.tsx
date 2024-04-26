a58b76bf4bbeccc63dc38df3221eda774e2b46bf
Md. Mehedi Hasan Nabil <mehedihasannabil49@gmail.com>
2024-04-26 12:31:08 +0600

    new setup

---
+++ components/dashboard/card/OverviewCard.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import ArroUp from "@/public/images/up.svg";
+import Image from "next/image";
+
+const OverviewCard = ({ imgSrc, title, number, gain }) => {
+  return (
+    <div className="overview_card">
+      <div className="title">
+        <Image src={imgSrc} alt={title} width="32" height="32" />
+        <div className="active-content">
+          <p className="active-text">{title}</p>
+          <h4>{number}</h4>
+          <div className="card-btn">
+            <Image src={ArroUp} width="16" height="16" alt="Arrow Up" />
+            <span>{gain}</span> gain this month
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default OverviewCard;
---
+++ components/dashboard/chart/PieChart.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import ReactApexChart from "react-apexcharts";
+
+const PieChart = () => {
+  const options = {
+    chart: {
+      width: 380,
+      type: "donut",
+    },
+    dataLabels: {
+      enabled: false,
+    },
+    responsive: [
+      {
+        breakpoint: 480,
+        options: {
+          chart: {
+            width: 200,
+          },
+          legend: {
+            show: false,
+          },
+        },
+      },
+    ],
+    legend: {
+      position: "right",
+      offsetY: 0,
+      height: 230,
+      labels: {
+        backgroundColor: ["#4153f6", "#a9c6ff"], // Background colors for the labels
+        formatter: function (val: any, opts: any) {
+          const sales = opts.w.globals.series[opts.seriesIndex];
+          return `<div><span>${val}</span><span> - Sales: ${sales}</span></div>`;
+        },
+      },
+    },
+    labels: [
+      '<div class="chart_num">Active models <span>12</span></div>',
+      '<div class="chart_num">Deactivated models<span>2</span></div>',
+    ],
+    colors: ["#4153F6", "#A9C6FF"],
+  };
+
+  return (
+    <div className="chart-wrap">
+      <div id="chart">
+        <ReactApexChart options={options} series={[12, 2]} type="donut" />
+      </div>
+    </div>
+  );
+};
+
+export default PieChart;
